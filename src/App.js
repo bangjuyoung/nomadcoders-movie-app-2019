85860e0269783f5f8cb6e340d68b39e089120694
bangjuyoung <7979456+bangjuyoung@users.noreply.github.com>
2020-12-04 21:16:08 +0900

    목업 데이터 생성

--- src/App.js
+++ src/App.js
@@ -1,28 +1,45 @@
 import './App.css';
 
+const movies = [1,2,3,4,5,6].map(item => (
+  {
+    id: 1,
+    title: 'Deported',
+    year: 2021,
+    generes: 'Comedy',
+    summary: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.',
+    poster: 'https://yts.mx/assets/images/movies/deported_2020/medium-cover.jpg'
+  }
+));
+
 function App() {
   return (
     <main className="container">
       <div className="movies">
         {
-          [1,2,3,4,5,6].map(item => (
-            <div className="movie">
-              <section className="movie-card">
-                <div className="movie-card__body">
-                  <h2 className="movie-card__title">Deported</h2>
-                  <p className="movie-card__caption">
-                    <time dateTime="2021">2021</time> Comedy
-                  </p>
-                  <p className="movie-card__text">
-                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
-                  </p>
-                </div>
-                <div className="movie-card__thumbnail">
-                  <img src="https://yts.mx/assets/images/movies/deported_2020/medium-cover.jpg" alt=""/>
-                </div>
-              </section>
-            </div>
-          ))
+          movies.map(movie => {
+
+            const {title, year, generes, summary, poster} = movie;
+
+            return (
+              <div className="movie">
+                <section className="movie-card">
+                  <div className="movie-card__body">
+                    <h2 className="movie-card__title">{title}</h2>
+                    <p className="movie-card__caption">
+                      <time dateTime="2021">{year}</time> {generes}
+                    </p>
+                    <p className="movie-card__text">
+                      {summary.slice(0, 100)}...
+                    </p>
+                  </div>
+                  <div className="movie-card__thumbnail">
+                    <img src={poster} alt=""/>
+                  </div>
+                </section>
+              </div>
+            )
+            }
+          )
         }
       </div>
     </main>
