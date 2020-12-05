ecb9e57c6636158bfe37c60b18b1cfd0ae7a2d06
bangjuyoung <7979456+bangjuyoung@users.noreply.github.com>
2020-12-05 20:35:37 +0900

    라우터 처리

--- src/App.js
+++ src/App.js
@@ -1,56 +1,32 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
-
+import { Link, Route } from "react-router-dom";
 import "./App.css";
-import MovieCard from "./components/MovieCard";
+import About from "./pages/About";
+import Movies from "./pages/Movies";
+import { Fragment } from "react";
 
 function App() {
-  const [isLoading, setIsLoading] = useState(true);
-  const [movies, setMovies] = useState(null);
-
-  useEffect(() => {
-    const moviesPromise = async () => {
-      try {
-        let moviesPromise = await axios.get(
-          "https://yts-proxy.now.sh/list_movies.json?sort_by=rating"
-        );
-        setMovies(moviesPromise["data"]["data"]["movies"]);
-      } catch (e) {
-        console.error(e);
-      } finally {
-        setTimeout(() => {
-          setIsLoading(false);
-        }, 2000);
-      }
-    };
-
-    moviesPromise();
-  }, []);
-
   return (
-    <main className="container">
-      <div className="movies">
-        {isLoading ? (
-          <div className="movie--loading">Loading...</div>
-        ) : (
-          movies &&
-          movies.map((movie) => {
-            return (
-              <div className="movie" key={movie.id}>
-                <MovieCard
-                  id={movie.id}
-                  title={movie.title}
-                  year={movie.year}
-                  genres={movie.genres}
-                  summary={movie.summary}
-                  poster={movie.medium_cover_image}
-                />
-              </div>
-            );
-          })
-        )}
-      </div>
-    </main>
+    <Fragment>
+      <nav className="container">
+        <div className="page-nav">
+          <Link to="/">Home</Link>
+          <Link to="/about">About</Link>
+          <Link to="/movies">Movies</Link>
+        </div>
+      </nav>
+
+      <main className="container">
+        <Route exact path="/">
+          Welcome Movie World
+        </Route>
+        <Route path="/about">
+          <About />
+        </Route>
+        <Route path="/movies">
+          <Movies />
+        </Route>
+      </main>
+    </Fragment>
   );
 }
 
---
+++ src/pages/Movies.js
@@ -0,0 +1,54 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+import MovieCard from "../components/MovieCard";
+
+function Movies(props) {
+  const [isLoading, setIsLoading] = useState(true);
+  const [movies, setMovies] = useState(null);
+
+  useEffect(() => {
+    const moviesPromise = async () => {
+      try {
+        let moviesPromise = await axios.get(
+          "https://yts-proxy.now.sh/list_movies.json?sort_by=rating"
+        );
+        setMovies(moviesPromise["data"]["data"]["movies"]);
+      } catch (e) {
+        console.error(e);
+      } finally {
+        setTimeout(() => {
+          setIsLoading(false);
+        }, 2000);
+      }
+    };
+
+    moviesPromise();
+  }, []);
+
+  return (
+    <div className="movies">
+      {isLoading ? (
+        <div className="movie--loading">Loading...</div>
+      ) : (
+        movies &&
+        movies.map((movie) => {
+          return (
+            <div className="movie" key={movie.id}>
+              <MovieCard
+                id={movie.id}
+                title={movie.title}
+                year={movie.year}
+                genres={movie.genres}
+                summary={movie.summary}
+                poster={movie.medium_cover_image}
+              />
+            </div>
+          );
+        })
+      )}
+    </div>
+  );
+}
+
+export default Movies;
