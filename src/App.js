9471e9ed46565859f0baa542d67bcace61b1824c
bangjuyoung <7979456+bangjuyoung@users.noreply.github.com>
2020-12-05 20:11:42 +0900

    MovieCard 속성 풀어놓기, prettier 패키지 추가

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 
-import './App.css';
+import "./App.css";
 import MovieCard from "./components/MovieCard";
 
 function App() {
-
   const [isLoading, setIsLoading] = useState(true);
   const [movies, setMovies] = useState(null);
 
   useEffect(() => {
     const moviesPromise = async () => {
       try {
-        let moviesPromise = await axios.get('https://yts-proxy.now.sh/list_movies.json?sort_by=rating');
-        setMovies(moviesPromise['data']['data']['movies']);
-
+        let moviesPromise = await axios.get(
+          "https://yts-proxy.now.sh/list_movies.json?sort_by=rating"
+        );
+        setMovies(moviesPromise["data"]["data"]["movies"]);
       } catch (e) {
         console.error(e);
       } finally {
@@ -25,24 +25,30 @@ function App() {
     };
 
     moviesPromise();
-
   }, []);
 
   return (
     <main className="container">
       <div className="movies">
-        {
-          isLoading
-            ? <div className="movie--loading">Loading...</div>
-            : movies && movies.map(movie => {
-              return (
-                <div className="movie" key={movie.id} >
-                  <MovieCard movie={movie} />
-                </div>
-              )
-            }
-          )
-        }
+        {isLoading ? (
+          <div className="movie--loading">Loading...</div>
+        ) : (
+          movies &&
+          movies.map((movie) => {
+            return (
+              <div className="movie" key={movie.id}>
+                <MovieCard
+                  id={movie.id}
+                  title={movie.title}
+                  year={movie.year}
+                  genres={movie.genres}
+                  summary={movie.summary}
+                  poster={movie.medium_cover_image}
+                />
+              </div>
+            );
+          })
+        )}
       </div>
     </main>
   );
--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,36 +1,30 @@
 import PropTypes from "prop-types";
 import "./MovieCard.css";
 
-function MovieCard(props) {
-  const { movie: {id, title, year, genres, summary, medium_cover_image:poster } } = props;
-
+function MovieCard({ id, title, year, genres, summary, poster }) {
   return (
     <section id={`movie${id}`} className="movie-card">
       <div className="movie-card__body">
         <h2 className="movie-card__title">{title}</h2>
         <p className="movie-card__caption">
-          <time dateTime="2021">{year}</time> {genres.join(', ')}
+          <time dateTime="2021">{year}</time> {genres.join(", ")}
         </p>
-        <p className="movie-card__text">
-          {summary.slice(0, 100)}...
-        </p>
+        <p className="movie-card__text">{summary.slice(0, 100)}...</p>
       </div>
       <div className="movie-card__thumbnail">
-        <img src={poster} alt=""/>
+        <img src={poster} alt="" />
       </div>
     </section>
   );
 }
 
 MovieCard.propTypes = {
-  movie: PropTypes.shape({
-    id: PropTypes.number.isRequired,
-    title: PropTypes.string.isRequired,
-    year: PropTypes.number,
-    genres: PropTypes.array,
-    summary: PropTypes.string,
-    medium_cover_image: PropTypes.string
-  })
-}
+  id: PropTypes.number.isRequired,
+  title: PropTypes.string.isRequired,
+  year: PropTypes.number,
+  genres: PropTypes.arrayOf(PropTypes.string),
+  summary: PropTypes.string,
+  medium_cover_image: PropTypes.string,
+};
 
 export default MovieCard;
